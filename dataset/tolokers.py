f014870fb70e539714fca9a17b266734d318133d
Suprim Nakarmi <suprimnakarmi@gmail.com>
2025-11-03 14:23:08 -0800

    Bug fix: removed class and used label key instead

--- dataset/tolokers.py
+++ dataset/tolokers.py
@@ -9,21 +9,22 @@ def load_tolokers():
 
     A = data["Network"]  
     X = data["Attributes"]                         
-    y_class = data["Class"].flatten()              
+    # y_class = data["Class"].flatten()              
     y_label = data["Label"].flatten()              
-    y_str = data["str_anomaly_label"].flatten()    # Redundant info in dataset, same as attribute anomaly (attr_anomaly_label)
-    y_attr = data["attr_anomaly_label"].flatten().astype(int)   
+    # y_str = data["str_anomaly_label"].flatten()    # Redundant info in dataset, same as attribute anomaly (attr_anomaly_label)
+    # y_attr = data["attr_anomaly_label"].flatten().astype(int)   
 
     print("Feature shape: ", X.shape)
-    print("Label shape: ", y_class.shape)  # using Class; change to y_label if preferred
-    print("Unique Labels (Class): ", np.unique(y_class))
+    # print("Label shape: ", y_class.shape)  # using Class
+    # print("Unique Labels (Class): ", np.unique(y_class))
     print("Unique Labels (Label): ", np.unique(y_label))
-    print("Unique labels in structural anomaly: ", np.unique(y_str))
-    print("count of structural anomaly: ", int(y_str.sum()))
-    print("Unique labels in attribute anomaly: ", np.unique(y_attr))
-    print("count of attribute anomaly: ", int(y_attr.sum()))
-    print("Percentage of anomaly: ", round(int(y_attr.sum())/len(y_attr) * 100, 4))
+    # print("Unique labels in structural anomaly: ", np.unique(y_str))
+    # print("count of structural anomaly: ", int(y_str.sum()))
+    # print("Unique labels in attribute anomaly: ", np.unique(y_attr))
+    # print("count of attribute anomaly: ", int(y_attr.sum()))
+    # print("Percentage of anomaly: ", round(int(y_attr.sum())/len(y_attr) * 100, 4))
+    print("Percentage of anomaly: ", round(int(y_label.sum())/len(y_label) * 100, 4))
     print("=============================================")
 
-    X_train, train_edge_index, y_train, X_test, test_edge_index, y_test=train_test_split(X,A,y_attr)
+    X_train, train_edge_index, y_train, X_test, test_edge_index, y_test=train_test_split(X,A,y_label)
     return X_train, train_edge_index, y_train, X_test, test_edge_index, y_test
